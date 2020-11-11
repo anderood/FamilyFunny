7de8a77f5f2eea818e90acd1c637c244f61e9e53
anderood <anderood@hotmail.com>
2020-11-11 13:58:46 +0000

    Concluido o Layout do Main

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -11,10 +11,29 @@ export default class Main extends Component{
                         <img src={avatar} alt="Imagem de Perfil"/>
                     </div>
                     <h1>Nome do Personagem</h1>
-                    <p>Alguma Descrição</p>
-                    <a href="">Acessar</a>
+                    <p>
+                    Mussum Ipsum, cacilds vidis litro abertis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Viva Forevis aptent taciti sociosqu ad litora torquent. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Casamentiss faiz malandris se pirulitá.
+                    </p>
+                    <div className="perfil-botao">
+
+                        <a href="">Acessar</a>
+                    </div>
+                </article>
+                <article>
+                    <div className="perfil-img">
+                        <img src={avatar} alt="Imagem de Perfil"/>
+                    </div>
+                    <h1>Nome do Personagem</h1>
+                    <p>
+                    Mussum Ipsum, cacilds vidis litro abertis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Viva Forevis aptent taciti sociosqu ad litora torquent. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Casamentiss faiz malandris se pirulitá.
+                    </p>
+                    <div className="perfil-botao">
+
+                        <a href="">Acessar</a>
+                    </div>
                 </article>
             </div>
+            
         )
     }
 }
